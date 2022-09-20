c7950e6f68363993f63bda61908f4221dc4d0953
Felipe Rudnik <82181914+zulark@users.noreply.github.com>
2022-09-19 22:34:18 -0300

    server

---
+++ server/prisma/migrations/20220918193854_create_game_table/migration.sql
@@ -0,0 +1,5 @@
+-- CreateTable
+CREATE TABLE "Game" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL
+);
---
+++ server/prisma/migrations/20220918193950_create_game_table/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - Added the required column `bannerUrl` to the `Game` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Game" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "bannerUrl" TEXT NOT NULL
+);
+INSERT INTO "new_Game" ("id", "title") SELECT "id", "title" FROM "Game";
+DROP TABLE "Game";
+ALTER TABLE "new_Game" RENAME TO "Game";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
